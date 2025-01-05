1728ea515b3f7cc211e0b499068f17abd0bc5c63
mdhuzef <huzefmd490@gmail.com>
2025-01-05 19:31:30 +0530

    palindrome

--- week-1/Week-1-assignment-with-tests/01-js/medium/palindrome.js
+++ week-1/Week-1-assignment-with-tests/01-js/medium/palindrome.js
@@ -7,7 +7,16 @@
 */
 
 function isPalindrome(str) {
+  let len=str.length;
+  for (let i=0;i<len/2;i++){
+    if(str[i]!=str[len-1-i]){
+      return false
+    }
+    
+  }
   return true;
 }
+// let str=""
+// console.log(isPalindrome(str))
 
 module.exports = isPalindrome;
